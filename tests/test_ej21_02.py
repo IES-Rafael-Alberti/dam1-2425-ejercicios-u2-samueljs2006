38dcc2bc988b48a5e61e59863d81d1851c9384a1
samuel <jssamuel@iesrafaelalberti.es>
2024-10-30 02:10:40 +0100

    test empezando

---
+++ tests/test_ej21_02.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# Agregar el directorio raíz del proyecto (practica6) al sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.condicionales.ej21_02 import comprobar_contraseña
+
+def test_comprobar_contraseña():
+    assert comprobar_contraseña("123abc") == "correcta"
+    assert comprobar_contraseña("jjdjfa") =="incorrecta"
